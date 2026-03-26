76b7670b148a3f0ccfbbd7e9bc311fee9954d124
emxth <randivemith.2003@gmail.com>
2026-03-26 15:39:03 +0530

    Add and modify for handle profile details

--- server/routes/projects.routes.js
+++ server/routes/projects.routes.js
@@ -7,15 +7,15 @@ const {
   deleteProject,
 } = require("../controllers/projects.controller");
 const { protect } = require("../middleware/auth.middleware");
-const { upload } = require("../middleware/upload.middleware");
+const { uploadImage } = require("../middleware/upload.middleware");
 
 const router = express.Router();
 
 router.get("/", getProjects);
 router.get("/:id", getProjectById);
 
-router.post("/", protect, upload.single("image"), createProject);
-router.put("/:id", protect, upload.single("image"), updateProject);
+router.post("/", protect, uploadImage.single("image"), createProject);
+router.put("/:id", protect, uploadImage.single("image"), updateProject);
 router.delete("/:id", protect, deleteProject);
 
 module.exports = router;
